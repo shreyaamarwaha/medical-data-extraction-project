dc919ac9838776579ba7c123bd983b4af2fa39bb
Shreya Marwaha <113081204+shreyaamarwaha@users.noreply.github.com>
2024-06-23 10:18:40 +0530

    Add files via upload

---
+++ backend_/src/extractor.py
@@ -0,0 +1,32 @@
+import util
+import pytesseract
+from pdf2image import convert_from_path
+from parser_prescription import PrescriptionParser
+from parser_patient_details import PatientDetailsParser
+
+poppler_path = '/usr/local/bin'
+
+def extract(file_path, file_format):
+    # step 1: extracting text from pdf file
+    pages = convert_from_path(file_path, poppler_path=poppler_path)
+    document_text = ''
+
+    if len(pages)>0:
+        page = pages[0]
+        processed_image = util.preprocess_image(page)
+        text = pytesseract.image_to_string(processed_image, lang='eng')
+        document_text = '\n' + text
+
+    # step 2: extract fields from text
+    if file_format == 'prescription':
+        extracted_data = PrescriptionParser(document_text).parse()
+    elif file_format == 'patient_details':
+        extracted_data = PatientDetailsParser(document_text).parse()
+    else:
+        raise Exception(f"Invalid document format: {file_format}")
+
+    return extracted_data
+
+if __name__ == '__main__':
+    data = extract('../resources/prescription/pre_2.pdf', 'prescription')
+    print(data)
